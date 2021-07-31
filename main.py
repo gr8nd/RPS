ae9246661ca32aca40d18ec191e71e8bd9f7a4f5
Matho <swtworm@gmail.com>
2021-07-31 22:24:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from rps import game_play
+
+
+def start_game():
+    game_play()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    start_game()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ rps.py
@@ -0,0 +1,59 @@
+"""In the game Rock Paper Scissors, two players simultaneously choose one of three
+options: rock, paper, or scissors. If both players choose the same option, then the
+result is a tie. However, if they choose differently, the winner is determined as
+follows:
+l
+Rock beats scissors, because a rock can break a pair of scissors.
+l
+Scissors beats paper, because scissors can cut paper.
+l
+Paper beats rock, because a piece of paper can cover a rock.
+Create a game in which the computer randomly chooses rock, paper, or scissors.
+Let the user enter a number 1, 2, or 3, each representing one of the three choices.
+Then, determine the winner. Save the application as RockPaperScissors.java.
+(In the chapter Characters, Strings, and the StringBuilder, you will modify the
+game so that the user enters a string for rock, paper, and scissors, rather than just
+entering a number.)"""
+
+from random import choice
+
+
+class RPS:
+    def __init__(self):
+        self.choices = [1, 2, 3]
+
+    def computer_play(self, cc):
+        self.cc = cc
+
+    def human_play(self, hc):
+        self.hc = hc
+
+    def check_winner(self):
+        if self.cc == self.hc:
+            print("It is a tie")
+        elif self.cc == 1 and self.hc == 2:
+            print("Human wins because paper can cover a rock")
+        elif self.cc == 2 and self.hc == 1:
+            print("Computer wins because paper can cover a rock")
+        elif self.cc == 1 and self.hc == 3:
+            print("Computer wins because rock can break scissors")
+        elif self.cc == 3 and self.hc == 1:
+            print("Human wins because rock can break scissors")
+        elif self.cc == 2 and self.hc == 3:
+            print("Human wins because scissors can cut paper")
+        elif self.cc == 3 and self.hc == 2:
+            print("Computer wins because scissors can cut paper")
+
+
+def game_play():
+    game = RPS()
+    for i in range(10):
+        human_choice = int(input("Enter your choice 1 for rock, 2 for paper and 3 for scissors: "))
+        game.human_play(human_choice)
+        print("Human has played", human_choice)
+
+        computer_choice = choice([1, 2, 3])
+        game.computer_play(computer_choice)
+        print("Computer has played", computer_choice)
+
+        game.check_winner()
